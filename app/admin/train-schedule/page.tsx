ef67fed6637302fe70c60cddba925e7256f597b4
Noel Sebu <noelsebu555@gmail.com>
2026-03-05 18:55:00 +0530

    page to view the opperations on train schedule created

---
+++ app/admin/train-schedule/page.tsx
@@ -0,0 +1,12 @@
+import ViewTrainSchedule from '@/components/trainSchedule/ViewTrainSchedule'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ViewTrainSchedule />
+    </div>
+  )
+}
+
+export default page
