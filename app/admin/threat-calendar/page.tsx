80fef3a492f70abbd5fc8ced4e44683657d6aa5b
Noel Sebu <noelsebu555@gmail.com>
2026-03-05 22:32:56 +0530

    import added threat-calender page

---
+++ app/admin/threat-calendar/page.tsx
@@ -0,0 +1,12 @@
+import ViewThreatCalendar from '@/components/threatCalendar/Viewthreatcalendar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ViewThreatCalendar />
+    </div>
+  )
+}
+
+export default page
