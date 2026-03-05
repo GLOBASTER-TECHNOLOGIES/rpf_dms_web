e182b8e0ed3a89eb0118cf4618a0135a2d5dce21
Noel Sebu <noelsebu555@gmail.com>
2026-03-05 11:02:31 +0530

    offier actions and view page create

---
+++ app/officer/page.tsx
@@ -0,0 +1,15 @@
+import OfficersView from '@/components/officer/OfficerView'
+import React from 'react'
+
+
+
+const page = () => {
+    return (
+        <div>
+            <OfficersView />
+        </div>
+
+    )
+}
+
+export default page
