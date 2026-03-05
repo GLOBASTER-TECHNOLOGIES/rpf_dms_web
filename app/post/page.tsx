bdd77c1de4a65471313fb9a0ec7232ea308c6877
Noel Sebu <noelsebu555@gmail.com>
2026-03-05 11:53:43 +0530

    post actions page created

---
+++ app/post/page.tsx
@@ -0,0 +1,12 @@
+import ViewPost from '@/components/post/Viewpost'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <ViewPost />
+        </div>
+    )
+}
+
+export default page
