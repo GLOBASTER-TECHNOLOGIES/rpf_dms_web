66b8fe66ed67d1da6f0fddaf942a8887a12a5a55
Noel Sebu <noelsebu555@gmail.com>
2026-03-09 23:20:34 +0530

    audio trainsicibe feature working

--- models/Debrief.ts
+++ models/Debrief.ts
@@ -3,44 +3,39 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IDebrief extends Document {
   staffId: mongoose.Types.ObjectId;
   shift: string;
-  voiceFile?: string;
+  date: Date;
   transcript?: string;
   summary?: string;
   observations?: string;
   improvements?: string;
-  dutyRating?: string;
   approved: boolean;
 }
 
 const DebriefSchema = new Schema<IDebrief>(
-{
-  staffId: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
+  {
+    staffId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    shift: String,
+
+    date: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+
+    transcript: String,
+
+    summary: String,
+
+    observations: String,
+
+    improvements: String,
   },
-
-  shift: String,
-
-  voiceFile: String,
-
-  transcript: String,
-
-  summary: String,
-
-  observations: String,
-
-  improvements: String,
-
-  dutyRating: String,
-
-  approved: {
-    type: Boolean,
-    default: false
-  }
-
-},
-{ timestamps: true }
+  { timestamps: true },
 );
 
 export default mongoose.models.Debrief ||
-mongoose.model<IDebrief>("Debrief", DebriefSchema);
+  mongoose.model<IDebrief>("Debrief", DebriefSchema);
