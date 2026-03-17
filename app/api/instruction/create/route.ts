9897b1a8b9a9ca8843bab61c32f44163bf9eba0b
Noel Sebu <noelsebu555@gmail.com>
2026-03-17 12:43:47 +0530

    send notification during instruction creation

--- app/api/instruction/create/route.ts
+++ app/api/instruction/create/route.ts
@@ -3,6 +3,7 @@ import dbConnect from "@/config/dbConnect";
 import Instruction from "@/models/Instruction.model";
 import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
+import admin from "@/config/firebaseAdmin";
 
 export async function POST(req: Request) {
   try {
@@ -16,19 +17,18 @@ export async function POST(req: Request) {
     if (!token) {
       return NextResponse.json(
         { success: false, message: "Unauthorized" },
-        { status: 401 }
+        { status: 401 },
       );
     }
 
     const decoded: any = jwt.verify(
       token,
-      process.env.JWT_ACCESS_SECRET as string
+      process.env.JWT_ACCESS_SECRET as string,
     );
 
     const validFrom = new Date(body.validFrom);
     const validTo = new Date(body.validTo);
 
-    // Ensure full day coverage
     validFrom.setHours(0, 0, 0, 0);
     validTo.setHours(23, 59, 59, 999);
 
@@ -39,17 +39,46 @@ export async function POST(req: Request) {
       createdBy: decoded.id,
     });
 
+    // 🔔 SEND NOTIFICATION
+    // 🔔 SEND NOTIFICATION (DIRECT TOKEN)
+    try {
+      const fcmToken =
+        "fqyrwcrPTaqvx1hPR4FK8v:APA91bGNHxbuuC1Y0HvMVUJsQ75YulO_cWGEueDu6C3mR4Yf7eCkpEm29bjZ9WsAVmL2IzEdHwIeoChvnHpDPW1YJRXQezMGBAxRML4jpgSZ0748PixzzKc";
+
+      await admin.messaging().send({
+        token: fcmToken, // 🔥 direct device
+
+        notification: {
+          title: "📢 New Instruction",
+          body: body.title || "A new instruction has been issued",
+        },
+
+        data: {
+          type: "instruction",
+          instructionId: instruction._id.toString(),
+        },
+
+        android: {
+          priority: "high",
+          notification: {
+            channelId: "high_importance_channel", // 🔥 must match Flutter
+          },
+        },
+      });
+    } catch (err) {
+      console.error("Notification Error:", err);
+    }
+
     return NextResponse.json({
       success: true,
       data: instruction,
     });
-
   } catch (error: any) {
     console.error(error);
 
     return NextResponse.json(
       { success: false, message: error.message },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
